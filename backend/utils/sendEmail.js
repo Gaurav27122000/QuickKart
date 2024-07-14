db8ef20c7b7667c00fafaf3cb12f037fbbfa3fee
Gaurav Goyal <gauravgoyal@192.168.1.6>
2024-07-15 01:18:01 +0530

    Fixed Reset Password

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,23 +2,22 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async (options)=>{
     var transport = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASSWORD
-        }
-      });
-
-      const message = {
-        from: `${process.env.SMTP_FROM_EMAIL}-${process.env.SMTP_FROM_NAME}`,
-        to: options.email,
-        subject: options.subject,
-        html: options.message,
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
       }
+    });
 
-      await transport.sendMail(message);
+    const message = {
+      from: `"${process.env.SMTP_FROM_NAME} " <${process.env.SMTP_FROM_EMAIL}>`,
+      to: options.email,
+      subject: options.subject,
+      html: options.message,
+    }
 
+    await transport.sendMail(message);
 }
 
 export default sendEmail;
